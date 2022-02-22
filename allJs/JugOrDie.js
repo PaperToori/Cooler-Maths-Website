baba0f810dc6ee2711b65cf599dfc0af19cae823
Karl Garratt <garratt.karl@elev.ga.ntig.se>
2022-02-22 11:02:51 +0100

    dabebepon

--- allJs/JugOrDie.js
+++ allJs/JugOrDie.js
@@ -8,6 +8,8 @@ document.getElementById("jug1-fill").addEventListener("click", fillJug1);
 document.getElementById("jug1-drain").addEventListener("click", drainJug1);
 document.getElementById("jug2-fill").addEventListener("click", fillJug2);
 document.getElementById("jug2-drain").addEventListener("click", drainJug2);
+document.getElementById("jug1-to-jug2").addEventListener("click", jug1to2);
+document.getElementById("jug2-to-jug1").addEventListener("click", jug2to1);
 
 function fillJug1() {
   document.getElementById("jug1-contents").innerHTML = jug1max;
@@ -30,11 +32,14 @@ function jug1to2() {
   cache = jug1 + jug2;
   if (cache > jug2max) {
     cache -= jug2max;
+    jug2 = jug2max;
+    jug1 = cache;
     document.getElementById("jug2-contents").innerHTML = jug2max;
     document.getElementById("jug1-contents").innerHTML = cache;
-  }
-  if (cache < jug2max) {
-    document.getElementById("jug2-contents").innerHTML = jug2 + cache;
+  } else if (cache <= jug2max) {
+    jug1 = 0;
+    jug2 = cache;
+    document.getElementById("jug2-contents").innerHTML = cache;
     document.getElementById("jug1-contents").innerHTML = 0;
   }
 }
@@ -42,11 +47,14 @@ function jug2to1() {
   cache = jug1 + jug2;
   if (cache > jug1max) {
     cache -= jug1max;
-    document.getElementById("jug1-contents").innerHTML = jug1max;
-    document.getElementById("jug2-contents").innerHTML = cache;
-  }
-  if (cache < jug1max) {
-    document.getElementById("jug1-contents").innerHTML = jug1 + cache;
-    document.getElementById("jug2-contents").innerHTML = 0;
+    jug1 = jug1max;
+    jug2 = cache;
+    document.getElementById("jug1-contents").innerHTML = jug1;
+    document.getElementById("jug2-contents").innerHTML = jug2;
+  } else if (cache <= jug1max) {
+    jug2 = 0;
+    jug1 = cache;
+    document.getElementById("jug1-contents").innerHTML = jug1;
+    document.getElementById("jug2-contents").innerHTML = jug2;
   }
 }
